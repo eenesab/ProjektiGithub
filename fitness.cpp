fe991652bc69f1f9cc9d31fbedcd33dcaaf3901e
AlbinBllaca <103896639+AlbinBllaca@users.noreply.github.com>
2025-02-14 20:08:52 +0100

    Latest version --

--- fitness.cpp
+++ fitness.cpp
@@ -7,7 +7,20 @@ float llogaritKalorite(int hapa) {
     return hapa * 0.04; // Mesatarisht 0.04 kalori per hap
 }
 
-// Funksion per te konvertuar kalorite ne minuta vrapimi (mesatarisht 10 kalori per minutë)
+// Funksion per te konvertuar kalorite ne minuta vrapimi (mesatarisht 10 kalori per minutï¿½)
 float konvertoNeVrapim(float kaloringaecja) {
     return kaloringaecja / 10.0;
+}
+
+// Funksion per te vleresuar nivelin e aktivitetit
+string vleresoAktivitetin(int hapa) {
+    if (hapa < 2000) return "Shume pak aktivitet. Duhet te levizesh me shume!";
+    else if (hapa < 6000) return "Aktivitet mesatar. Mund te permiresohesh!";
+    else if (hapa < 10000) return "Shume mire! Vazhdoni keshtu!";
+    else return "Super! Po jeton shendetshem!";
+}
+
+// Funksion per te llogaritur kalorite e djegura nga ushtrimet fizike
+float llogaritKaloriUshtrime(float koheMinuta, float intensitet) {
+    return koheMinuta * intensitet * 5.0; // Mesatarisht 5 kalori per minute per nivel mesatar
 }
